c37fcf234425a268438129cc506a711fcf75aacf
murugesh <murugesh@meliosystems.com>
2018-07-07 07:22:34 +0000

    added script for mysql,apache

---
+++ watch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+service=mysql
+
+if (( $(ps -ef | grep -v grep | grep $service | wc -l) > 0 ))
+then
+echo "$service is running!!!"
+else
+/etc/init.d/$service start
+fi
+
+service=apache2
+
+if (( $(ps -ef | grep -v grep | grep $service | wc -l) > 0 ))
+then
+echo "$service is running!!!"
+else
+/etc/init.d/$service start
+fi
